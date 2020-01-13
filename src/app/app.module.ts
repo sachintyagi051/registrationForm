0dfb3474be8d97353344b80960fa0c5d117863e5
sachintyagi051 <sachin@zenways.io>
2020-01-13 13:27:15 +0530

    Registration Form Assignment in Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from "@angular/core";
+import {
+  Validators,
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  FormControl
+} from "@angular/forms";
+
+@Component({
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
+})
+export class RegisterComponent implements OnInit {
+  [x: string]: any;
+
+  form: FormGroup;
+  address: FormArray;
+
+  disabledAgreement: boolean = true;
+  changeCheck(event) {
+    this.disabledAgreement = !event.checked;
+  }
+
+  keyPress(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+
+    let inputChar = String.fromCharCode(event.charCode);
+    if (event.keyCode != 8 && !pattern.test(inputChar)) {
+      event.preventDefault();
+    }
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.dialogFormGroup = this.fb.group({
+      confirmAction: [false]
+    });
+
+    this.form = this.fb.group({
+      firstname: ["", Validators.required],
+      lastname: ["", Validators.required],
+      username: ["", Validators.required],
+      email: [""],
+      address: this.fb.array([this.addressLines()]),
+      checkbox: [""]
+    });
+
+    console.log("form", this.form);
+  }
+
+  onSubmit() {
+    console.log("value:", this.form.value);
+  }
+
+  formArrayControls() {
+    let fA = this.form.get("address") as FormArray;
+    return fA.controls;
+  }
+
+  addressLines(): FormGroup {
+    return this.fb.group({
+      addressLine1: ["", Validators.required],
+      addressLine2: ["", Validators.required],
+      city: ["", Validators.required],
+      state: ["", Validators.required],
+      pincode: [
+        "",
+        [Validators.required, Validators.minLength(6), Validators.maxLength(6)]
+      ]
+    });
+  }
+
+  addNewAddress() {
+    this.addAddressLines();
+  }
+
+  removeAddress(addres: any) {
+    let index = this.address.controls.indexOf(addres);
+    this.address.removeAt(index);
+  }
+
+  addAddressLines(): void {
+    this.address = this.form.get("address") as FormArray;
+    this.address.push(this.addressLines());
+  }
+}
